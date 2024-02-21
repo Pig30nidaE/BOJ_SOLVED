48ec2a857d51a2c0eb5b2c49ecb2ada7a4e87f9e
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-02-22 00:34:55 +0900

    [Silver II] Title: 아침 태권도, Time: 756 ms, Memory: 90588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29197. 아침 태권도/아침 태권도.py
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.py
@@ -1,26 +1,44 @@
 import sys
-from math import sqrt, pow
+from math import sqrt
 input = sys.stdin.readline
 
 
+def getQuad(x: int, y: int)->int:
+	if x > 0 and y > 0:
+		return 1
+	elif x < 0 and y > 0:
+		return 2
+	elif x > 0 and y < 0:
+		return 3
+	elif x < 0 and y < 0:
+		return 4
+	elif x > 0 and y == 0:
+		return 11
+	elif x < 0 and y == 0:
+		return -11
+	elif x == 0 and y > 0:
+		return 22
+	elif x == 0 and y < 0:
+		return -22
+	else:
+		return 0
 
 def getDistance(x: int, y: int)->float:
-	return sqrt(pow(x, 2) + pow(y, 2))
-
-def isOnTheLine(slope: float, x: int, y: int)->bool:
-	if y == slope * x:
-		return True
-	return False
+	return sqrt(x*x + y*y)
 
 n = int(input())
 slopeDict = dict()
 for _ in range(n):
 	x, y = map(int, input().split())
-	slope = y / x
 	try:
-		if getDistance(x, y) < getDistance(slopeDict[slope][0], slopeDict[slope][1]):
+		slope = y / x
+	except ZeroDivisionError:
+		slope = "inf"
+	quad = getQuad(x, y)
+	try:
+		if getDistance(x, y) < getDistance(slopeDict[(slope, quad)][0], slopeDict[(slope, quad)][1]):
 			slopeDict[slope][0] = x
 			slopeDict[slope][1] = y
 	except KeyError:
-		slopeDict[slope] = [x, y]
+		slopeDict[(slope, quad)] = [x, y]
 print(len(slopeDict.keys()))
