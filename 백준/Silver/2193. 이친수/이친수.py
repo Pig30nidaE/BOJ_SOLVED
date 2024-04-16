6dfadaa2aa3d00798d472028d11e5e9fdae21557
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-04-16 19:05:23 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -17,12 +17,6 @@ dp[2] = (1, 0)
 for i in range(3, n):
 	num = dp[i - 1]
 	dp[i] = (num[0] + num[1], num[0])
-		#if num[-1] == '0':
-		#	dp[i].append(num + '0')
-		#	dp[i].append(num + '1')
-		#else:
-		#	dp[i].append(num + '0')
-
 num = dp[n - 1]
 dp[n] = (num[0] + num[1], num[0])
 print(dp[n][0] + dp[n][1])
