89f3cb8e687926aabc267cb5238ef6579b4da364
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-04-16 21:19:56 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 284 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,25 @@
+from sys import stdin
+input = stdin.readline
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = dict()
+
+def recur(idx: int, n: int, sum: int, nums: list)->int:
+	global dp
+	if idx in dp.keys():
+		return dp[idx]
+	max = 0
+	for i in range(idx + 1, n):
+		if nums[i] > nums[idx]:
+			res = recur(i, n, sum + nums[i], nums)
+			if res > max:
+				max = res
+	dp[idx] = nums[idx] + max
+	return dp[idx]
+
+for i in range(n):
+	recur(i, n, 0, nums)
+print(max(dp.values()))
