5bd795deb479bf8ddd286c0e5e9c899db7e9b953
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-05-20 17:48:54 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6612 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,5 +1,6 @@
-from sys import stdin
+from sys import stdin, stdout
 input = stdin.readline
+print = stdout.write
 
 n = int(input())
 l = [0 for i in range(10001)]
@@ -7,4 +8,4 @@ for i in range(n):
 	l[int(input())] += 1
 for i in range(10001):
 	for j in range(l[i]):
-		print(i)
+		print(str(i) + "\n")
