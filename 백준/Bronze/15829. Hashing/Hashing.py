652a76f4097daf5ece1bd57faec9bcfdd263e14c
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-05-20 16:44:17 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 34736 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -10,6 +10,6 @@ for i in alpha:
 	al_dict[i] = num
 	num += 1
 k = 0
-for i in range(1, len(string) + 1):
+for i in range(1, l + 1):
 	k += al_dict[string[i - 1]] * pow(31, i - 1)
-print(k)
+print(k%1234567891)
