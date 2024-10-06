881402af68f002424842690d1948ba39b52bedce
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-10-06 23:05:34 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,10 +1,10 @@
+from sys import stdin
+input = lambda : stdin.readline()
+
+
 a = int(input())
 b = int(input())
 c = int(input())
-num = str(a*b*c)
-temp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-for i in num:
-	temp[int(i)] += 1
-for i in temp:
-	print(i)
-	
+
+for i in range(10):
+    print(f'{str(a * b * c).count(str(i))}')
