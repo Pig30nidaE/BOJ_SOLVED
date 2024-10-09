bb991883b4a17c748259844b31cf7af6d25fe2f6
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-10-09 15:59:48 +0900

    [Silver II] Title: 스택 수열, Time: 172 ms, Memory: 36560 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,39 +1,28 @@
 from sys import stdin
-from collections import deque
 input = stdin.readline
 
+
 n = int(input())
-sequence = deque()
+seq = []
 for _ in range(n):
-	sequence.append(int(input()))
+    seq.append(int(input()))
+stack = []
+topop = 0
+answer = []
+try:
+    for i in range(1, n + 1):
+        stack.append(i)
+        answer.append('+')
+        while stack and stack[-1] == seq[topop]:
+            stack.pop()
+            answer.append('-')
+            topop += 1
+except IndexError:
+    print("NO")
+if stack:
+    print("NO")
+else:
+    for i in answer:
+        print(i)
 
-if n == 1:
-	for i in range(1, sequence[0] + 1):
-		print("+")
-	print("-")
-	exit()
-
-stack = list()
-
-start = 1
-i = 1
-ans = ""
-while sequence:
-	num = sequence.popleft()
-	while i <= n + 1:
-		if i <= num:
-			stack.append(i)
-			i += 1
-			ans += "+\n"
-		elif i > num:
-			ans += "-\n"
-			if stack: 
-				stackTop = stack.pop()
-			if stackTop != num:
-				print("NO")
-				exit()
-			if sequence:
-				num = sequence.popleft()
-			else: 
-				i += 1
-print(ans)
+    
