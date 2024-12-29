bf26de4122f180aaa0fc12e000a83f2f1756ea4f
Pig30nidaE <79036425+Pig30nidaE@users.noreply.github.com>
2024-12-29 15:13:22 +0900

    [Silver II] Title: 최대 힙, Time: 464 ms, Memory: 116380 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,57 @@
+from sys import stdin
+from collections import deque
+input = lambda : int(stdin.readline())
+
+if __name__ == '__main__':
+    n = int(input())
+
+    max_heap = deque()
+    for _ in range(n):
+        command = input()
+        if command == 0:
+            if max_heap:
+                print(max_heap.popleft())
+                max_heap.rotate(1)
+                idx = 0
+                child1 = idx * 2 + 1
+                child2 = idx * 2 + 2
+                while child1 < len(max_heap) or child2 < len(max_heap):
+                    if child1 < len(max_heap) and child2 < len(max_heap):
+                        if max_heap[child1] < max_heap[child2]:
+                            if max_heap[idx] < max_heap[child2]:
+                                max_heap[idx], max_heap[child2] = max_heap[child2], max_heap[idx]
+                                idx = child2
+                            else:
+                                break
+                        else:
+                            if max_heap[idx] < max_heap[child1]:
+                                max_heap[idx], max_heap[child1] = max_heap[child1], max_heap[idx]
+                                idx = child1
+                            else:
+                                break
+                    elif child1 < len(max_heap):
+                        if max_heap[child1] > max_heap[idx]:
+                            max_heap[idx], max_heap[child1] = max_heap[child1], max_heap[idx]
+                            idx = child1
+                        else:
+                            break
+                    elif child2 < len(max_heap):
+                        if max_heap[child2] > max_heap[idx]:
+                            max_heap[idx], max_heap[child2] = max_heap[child2], max_heap[idx]
+                            idx = child2
+                        else:
+                            break
+                    child1 = idx * 2 + 1
+                    child2 = idx * 2 + 2
+            else:
+                print(0)
+        else:
+            max_heap.append(command)
+            idx = len(max_heap) - 1
+            parrent = (idx - 1) // 2
+            while parrent >= 0 and idx >= 0 and idx != parrent and max_heap[parrent] < max_heap[idx]:
+                max_heap[idx], max_heap[parrent] = max_heap[parrent], max_heap[idx]
+                idx = parrent
+                parrent = (idx - 1) // 2
+        
+
